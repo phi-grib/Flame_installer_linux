602cd89201ff307194354a3cb1ff13ed8c0c2b74
Eric March <eric.march91@gmail.com>
2020-07-09 13:45:05 +0200

    minor changes

--- flame_api.sh
+++ flame_api.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-conda init
-source activate flame
 apidir=$(find $HOME/flame_core -type d -name flame_API)
 cd "${apidir}/flame_api"
+conda init
+source activate flame
 python manage.py runserver --noreload | xdg-open "http://127.0.0.1:8000/"
