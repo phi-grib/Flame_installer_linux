a9d169eb2a10722cfebcf53257a0c8e9611d566b
Eric March <eric.march91@gmail.com>
2020-07-09 12:44:51 +0200

    minor changes

--- installer.sh
+++ installer.sh
@@ -52,7 +52,7 @@ else
 fi
 
 # Adding Flame executable in desktop
-installer_dir=$(find $HOME -name Flame_installer_linux)
+installer_dir=$(locate -b Flame_installer_linux)
 cd ${installer_dir}
 cp exec/Flame.desktop $HOME/Desktop
 
