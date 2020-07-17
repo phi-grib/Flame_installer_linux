eef1452d3958771f078181cd943f464b7e45d900
Eric March <eric.march91@gmail.com>
2020-07-17 18:01:08 +0200

    source .bashrc added

--- flame_api.sh
+++ flame_api.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-apidir=$(find $HOME/flame_core -type d -name flame_API)
-cd "${apidir}/flame_api"
-conda init
+cd $HOME/flame_core/flame_API/flame_api
+conda init bash
+source $HOME/.bashrc
 source activate flame
+echo "------ Using Flame `which flame` -----------"
+flame -c config
 python manage.py runserver --noreload | xdg-open "http://127.0.0.1:8000/"
