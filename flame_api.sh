589111c90ceff9b70dec63e863599d8b74393ed3
Eric March <eric.march91@gmail.com>
2020-10-20 18:48:11 +0200

    minor changes

--- flame_api.sh
+++ flame_api.sh
@@ -4,7 +4,8 @@ cd $HOME/flame_core/flame_API/flame_api
 source $HOME/.bashrc
 conda init bash
 source activate flame
+pypath=$(which python)
+settings=${HOME}/flame_core/flame_API/flame_api/
 echo "------ Using Flame `which flame` -----------"
 flame -c config
-# python manage.py runserver --noreload | xdg-open "http://127.0.0.1:8000/"
-python manage.py runserver | xdg-open "http://127.0.0.1:8000/"
+python manage.py runserver --noreload --pythonpath ${pypath}  --settings ${settings}  | xdg-open "http://127.0.0.1:8000/"
